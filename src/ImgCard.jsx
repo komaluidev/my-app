a94b6c49eca32bc6102df186ddedabdaa68be6bc
Komal Sharma <ksharma@intelliveer.com>
2024-05-19 18:16:22 +0530

    react-changes

---
+++ src/ImgCard.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const ImgCard = () => {
+  const [cards, setCards] = useState([]);
+
+  return (
+    <div className="flex ">
+      <emplyee>name="mamam", role="reel", wear="eee"</emplyee>
+    </div>
+  );
+};
+
+export default ImgCard;
